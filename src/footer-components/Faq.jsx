391880b768c4aa856b19c23769cb4584ec4cd6cf
justpeterjp1 <oluwagbotemiojopeter10@gmail.com>
2025-12-12 02:24:02 +0100

    added footer sections

---
+++ src/footer-components/Faq.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Faq = () => {
+  return (
+    <section>
+       <div>
+        <h1>question?</h1>
+        <p>Answer.</p>
+        <h1>question?</h1>
+        <p>Answer.</p>
+        <h1>question?</h1>
+        <p>Answer.</p>
+        <h1>question?</h1>
+        <p>Answer.</p>
+        <h1>question?</h1>
+        <p>Answer.</p>
+        </div> 
+
+    </section>
+  )
+}
+
+export default Faq
